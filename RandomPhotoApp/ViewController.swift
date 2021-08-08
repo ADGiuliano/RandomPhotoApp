522d9874b27dc3bc9a7a132ddb58676065cfc808
Andrea Domenico Giuliano <draconian892@gmail.com>
2021-08-08 23:22:22 +0200

    Initial Commit

---
+++ RandomPhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhotoApp
+//
+//  Created by Andrea Domenico Giuliano on 08/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
