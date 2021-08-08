3c5fa02d38c9480a93bc07e76ca196044dca6962
Andrea Domenico Giuliano <draconian892@gmail.com>
2021-08-09 00:32:17 +0200

    Add randomPhoto core function

--- RandomPhotoApp/ViewController.swift
+++ RandomPhotoApp/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+         
+        
+        return imageView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let appTitle = "Random Photo"
+        title = appTitle
+        
+        view.backgroundColor = .systemGray
+        view.addSubview(imageView)
+        
+        let xValue = 0
+        let yValue = 0
+        let widthValue = 320
+        let heightValue = 320
+        
+        imageView.frame = CGRect(x: xValue,
+                                 y: yValue,
+                                 width: widthValue,
+                                 height: heightValue)
+        
+        imageView.center = view.center
+        
+        getRandomPhoto()
     }
-
-
+    
+    func getRandomPhoto()
+    {
+        let urlString = "https://source.unsplash.com/random/600x600"
+        
+        //  The ! indicate that we assume that the url is valid (exist)
+        let url = URL(string: urlString)!
+        
+        // if the data is null, return null (return)
+        guard let imageData = try? Data(contentsOf: url) else {
+            return
+        }
+        
+        imageView.image =  UIImage(data: imageData)
+        
+    }
 }
 
